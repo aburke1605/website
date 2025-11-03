4c8e6686a316136e7383daf3c9927fcf08ea3e0e
aburke1605 <aodhanburke@hotmail.com>
2025-11-03 10:10:56 +0000

    add unique ip counter

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
+
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
+
+count = 0
+client_ips = []
+
 @app.route("/")
 def index():
+    global count, client_ips
+    client_ip = request.remote_addr
+    if client_ip not in client_ips:
+        count += 1
+        client_ips.append(client_ip)
     return render_template("index.html")
 
 @app.route("/snake")
@@ -17,6 +29,10 @@ def collider():
 @app.route("/Collider.data")
 def collider_data():
     return send_from_directory('static', "emscripten/Collider.data")
+
+@app.route("/visits")
+def visits():
+    return f"visits: {count}"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
