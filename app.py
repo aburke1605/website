d5c12cf6639c1933cfc4973d6ebc7900c56f9174
aburke1605 <aodhanburke@hotmail.com>
2026-01-22 14:39:54 +1300

    run locally on all hosts

--- app.py
+++ app.py
@@ -75,4 +75,4 @@ def collider_data():
     return send_from_directory('static', "emscripten/Collider.data")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
